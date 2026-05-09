1c1d1afdcabfa4bd7200071bee4540a0996b0e98
BugTracker <hiddenchest2010@gmail.com>
2026-05-09 20:12:23 +0300

    Delete Untitled.sql

--- Untitled.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE API INTEGRATION my_github_api_integration
-  API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('https://github.com/BugTracker-BH')
-  API_USER_AUTHENTICATION = (
-    TYPE = SNOWFLAKE_GITHUB_APP
-  )
-  ENABLED = TRUE;
